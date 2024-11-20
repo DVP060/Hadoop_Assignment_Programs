3e2a560949b7c6769b9302bb89b980d9a8c64e57
DVP060 <dvp060723@gmail.com>
2024-11-20 14:27:04 +0530

    changing main function to all files

--- SeventhProgram.java
+++ SeventhProgram.java
@@ -9,7 +9,10 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
 
 public class SeventhProgram {
 	public static class Map extends Mapper<LongWritable,Text,Text,IntWritable>{
@@ -68,17 +71,24 @@ public class SeventhProgram {
 	public static void main(String args[]) throws Exception {
 		Configuration conf = new Configuration();
 		Job job = Job.getInstance(conf,"SeventhProgram");
-		
 		job.setJarByClass(SeventhProgram.class);
 		job.setMapperClass(Map.class);
 		job.setReducerClass(Reduce.class);
 		
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 		
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		Path outputPath = new Path(args[1]);
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		
-		System.exit(job.waitForCompletion(true)?0:1);
+		outputPath.getFileSystem(conf).delete(outputPath, true);
+		System.exit(job.waitForCompletion(true)?1:0);
 	}
 }
